004b4fc2155ccc8da6950ea761b331b43009f212
ankitsingh9162 <149968562+ankitsingh9162@users.noreply.github.com>
2025-04-02 17:40:37 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def list_files(directory):
-    """Lists all files in the given directory."""
-    if os.path.exists(directory):
-        return os.listdir(directory)
-    else:
-        return []
